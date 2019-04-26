e4a29176a3c5b90ccd4aa97c89c9b03e2493946d
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-26 21:10:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,12 @@
 def key_for_min_value(name_hash)
       min_value = 0
       min_key = nil
-      name_hash.each do |key, value|
+      name_hash.collect do |key, value|
         if value < min_value || min_value == 0
           min_value = value
           min_key = key
         end
-      min_key
-  end 
+          min_key
+      end 
 end
 
